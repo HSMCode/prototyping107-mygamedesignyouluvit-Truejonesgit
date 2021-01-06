8f6e1d0f246667b74df527f1b1fb68e6c31e509b
Truejonesgit <57531141+Truejonesgit@users.noreply.github.com>
2021-01-06 06:57:22 +0100

    add 3 lives mechanic

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+
+    public int lives;
+    public GameObject zero;
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (lives == 3)
+        {
+            three.gameObject.SetActive(true);
+            two.gameObject.SetActive(false);
+            one.gameObject.SetActive(false);
+            zero.gameObject.SetActive(false);
+
+        }
+        else if (lives == 2)
+        {
+            three.gameObject.SetActive(false);
+            two.gameObject.SetActive(true);
+            one.gameObject.SetActive(false);
+            zero.gameObject.SetActive(false);
+        }
+        else if (lives == 1)
+        {
+            three.gameObject.SetActive(false);
+            two.gameObject.SetActive(false);
+            one.gameObject.SetActive(true);
+            zero.gameObject.SetActive(false);
+        }
+        else if (lives == 0)
+        {
+            three.gameObject.SetActive(false);
+            two.gameObject.SetActive(false);
+            one.gameObject.SetActive(false);
+            zero.gameObject.SetActive(true);
+        }
+    }
+}
